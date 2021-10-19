793a9a8ca22a5dbb91bea2c3868348b1cb57e7b1
BrianSayen <blu3fire89@gmail.com>
2021-10-19 17:37:44 +0800

    updates

--- src/components/CheckoutPage/InventoryCheckoutItem.js
+++ src/components/CheckoutPage/InventoryCheckoutItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function InventoryCheckoutItem({item}) {
+  const totalPrice = item.cart.count * item.cart.price
   return (
     <div className="inventoryCheckout__item">
       <input className="inventoryCheckoutItem__checkBox" type="checkbox" />
@@ -8,11 +9,11 @@ function InventoryCheckoutItem({item}) {
         <img className="inventoryCheckoutItem__image" src={item.cart.food.food.image} alt={item.cart.food.food.label} />
         <div className="inventoryCheckoutItem__text">
           <p>{item.cart.food.food.label}</p>
-          <p>x {item.cart.count}</p>
-          <p>P {item.cart.price}</p>
-
+          <p>x {item.cart.count}pcs</p>
+          <p>P {totalPrice}.00</p>
         </div>
       </div>
+      <button>Remove</button>
     </div>
   )
 }
